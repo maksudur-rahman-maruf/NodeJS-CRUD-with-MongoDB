6253c6de670fbf5a5dcd0fd666c2489ac13b7002
Maksudur Rahman Maruf <maruf.bsnm@gmail.com>
2021-07-09 22:28:28 +0600

    first commit

---
+++ controllers/EmployeeController.js
@@ -0,0 +1,100 @@
+const Employee = require('../models/Employee')
+
+// Show the list of Employees
+const index = (req, res, next) => {
+    Employee.find()
+    .then(response => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
+// Show single Employee
+const show = (req, res, next) => {
+    let employeeID = req.body.employeeID;
+    Employee.findById(employeeID)
+    .then(response => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+     })
+}
+
+// Add an Employee
+const store = (req, res, next) => {
+    let employee = new Employee({
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age:req.body.age
+    })
+    employee.save()
+    .then(response => {
+        res.json({
+            message: 'Employee Added Successfully!'
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
+// Update an Employee
+const update = (req, res, next) => {
+    let employeeID = req.body.employeeID
+
+    let updateData = {
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age:req.body.age
+    }
+
+    Employee.findByIdAndUpdate(employeeID, {$set: updateData})
+    .then(() => {
+        res.json({
+            message: 'Employee Updated Successfully!'
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
+// Delete an Employee
+const destroy = (req, res, next) => {
+    let employeeID = req.body.employeeID
+    Employee.findByIdAndRemove(employeeID)
+    .then(() => {
+        res.json({
+            message: 'Employee deleted successfully!'
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
+module.exports = {
+    index, show, store, update, destroy
+}
+
